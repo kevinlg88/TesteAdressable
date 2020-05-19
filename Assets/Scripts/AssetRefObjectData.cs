98cc1f85337b32ca70e0ed67c3b6cd156c424fad
Kevin Grandez <kevinlg08@gmail.com>
2020-05-19 20:13:26 -0300

    project adressable

---
+++ Assets/Scripts/AssetRefObjectData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class AssetRefObjectData : MonoBehaviour
+{
+    [SerializeField] private AssetReference _sqrAref;
+    [SerializeField] private List<AssetReference> _references = new List<AssetReference>();
+
+
+    [SerializeField] private List<GameObject> _completeObj = new List<GameObject>();
+
+    private void Awake () 
+    {
+        _references.Add(_sqrAref);    
+
+        StartCoroutine(LoadAndWaitUntilComplete());
+    }
+
+    private IEnumerator LoadAndWaitUntilComplete()
+    {
+       yield return AssetRefLoader.CreateAssetsAddToList(_references, _completeObj);
+    }
+
+}
