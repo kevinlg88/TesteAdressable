2820cdb375c2824b7d73eebad4b5ae4d1ca51b58
Kevin Grandez <kevinlg08@gmail.com>
2020-12-05 18:37:14 -0300

    adressables

--- Assets/Scripts/AssetRefObjectData.cs
+++ Assets/Scripts/AssetRefObjectData.cs
@@ -1,26 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class AssetRefObjectData : MonoBehaviour
 {
-    [SerializeField] private AssetReference _sqrAref;
-    [SerializeField] private List<AssetReference> _references = new List<AssetReference>();
+    public Image image;
+    public Text text;
 
+    public Text textID;
+    public AssetReference assetref;
 
-    [SerializeField] private List<GameObject> _completeObj = new List<GameObject>();
-
-    private void Awake () 
+    private AsyncOperationHandle handle;
+    private void Start () 
     {
-        _references.Add(_sqrAref);    
+        //Addressables.UpdateCatalogs(null, true);
 
-        StartCoroutine(LoadAndWaitUntilComplete());
+        //Debug.Log("Asset GUID: " + assetref.AssetGUID);
+        AsyncOperationHandle<List<string>> list = Addressables.CheckForCatalogUpdates(true);
+        StartCoroutine(CheckUpdates(list));
+
     }
 
-    private IEnumerator LoadAndWaitUntilComplete()
+    IEnumerator CheckUpdates(AsyncOperationHandle<List<string>> upHandle)
     {
-       yield return AssetRefLoader.CreateAssetsAddToList(_references, _completeObj);
+
+        while(!upHandle.IsDone)
+        {
+            textID.text = "Checking updates..." + upHandle.PercentComplete;
+            image.color = Color.yellow;
+            yield return null;
+        }
+
+        image.color = Color.green;
+        foreach (var item in upHandle.Result)
+        {
+            textID.text +=", " + item;   
+        }
+
+        handle = Addressables.DownloadDependenciesAsync(assetref);
+        StartCoroutine(DownloadAsset(handle));
+
+    }
+
+    IEnumerator DownloadAsset (AsyncOperationHandle handle)
+    {
+        while(!handle.IsDone)
+        {
+            text.text = handle.Status.ToString() + " - " + handle.PercentComplete;
+            image.color = Color.yellow;
+            yield return null;
+        }
+        image.color = Color.blue;
+        handle =  Addressables.InstantiateAsync(assetref,new Vector3(0,0,0), Quaternion.identity);
+        StartCoroutine(InstAsync(handle));
+    }
+
+    IEnumerator InstAsync (AsyncOperationHandle handle)
+    {
+        while(!handle.IsDone)
+        {
+            text.text = handle.Status.ToString() + " - " + handle.PercentComplete;
+            image.color = Color.yellow;
+            yield return null;
+        }
+        GameObject result = handle.Result as GameObject;
+        text.text = "Result: " + result.name;
+        if(result != null)
+        {
+            image.color = Color.green;
+            text.text = "Name: " + result.name;
+        }
+        
+        Caching.ClearCache();
+        assetref.ReleaseAsset();
     }
 
 }
